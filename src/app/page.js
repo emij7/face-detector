3a25e9bb147b1c65652f5ab6ae490bdaf95a319f
Emiliano Juarez <75501977+emij7@users.noreply.github.com>
2023-07-29 21:49:34 -0300

    Feat: Particles bg added

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,33 @@
+"use client";
+//Components
+import ImageLinkForm from "@/components/ImageLinkForm/ImageLinkForm";
 import Logo from "@/components/logo/logo";
+import Rank from "@/components/Rank/Rank";
 import Navigation from "@/components/navigation/navigation";
+//Libraries
 import "tachyons";
+import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
+import { particleOptions } from "@/utils/particlesConfig";
 
 export default function Home() {
+  const options = particleOptions;
+  const particlesInit = async (main) => {
+    await loadFull(main);
+  };
   return (
     <main>
+      <Particles
+        id="tsparticles"
+        options={options}
+        height="100vh"
+        width="100vw"
+        init={particlesInit}
+      />
       <Navigation />
       <Logo />
+      <Rank />
+      <ImageLinkForm />
     </main>
   );
 }
