6a3ec7bb85bf37a6eea55745301c0e5aaa4003ca
Emiliano Juarez <75501977+emij7@users.noreply.github.com>
2023-07-30 09:38:11 -0300

    Create clarifaiConfig.js

---
+++ src/utils/clarifaiConfig.js
@@ -0,0 +1,16 @@
+export const CLARIFAI_CONFIG_OBJECT = {
+  PAT: "0fc2fc7d8cb743aa91dfa63f9ffd1092",
+  USER_ID: "clarifai",
+  APP_ID: "main",
+  MODEL_ID: "face-detection",
+  MODEL_VERSION_ID: "6dc7e46bc9124c5c8824be4822abe105",
+};
+const PAT = "YOUR_PAT_HERE";
+// Specify the correct user_id/app_id pairings
+// Since you're making inferences outside your app's scope
+const USER_ID = "clarifai";
+const APP_ID = "main";
+// Change these to whatever model and image URL you want to use
+const MODEL_ID = "face-detection";
+const MODEL_VERSION_ID = "6dc7e46bc9124c5c8824be4822abe105";
+const IMAGE_URL = "https://samples.clarifai.com/metro-north.jpg";
