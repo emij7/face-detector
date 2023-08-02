d2cd684c26bb3465fc67ee857ced78b5610e8aca
Emiliano Juarez <75501977+emij7@users.noreply.github.com>
2023-08-02 20:18:18 -0300

    Fix: unnecessary code removed

--- src/app/page.js
+++ src/app/page.js
@@ -1,32 +1,12 @@
 "use client";
-//React
-import { useCallback, useState } from "react";
 
 //Libraries
 import "tachyons";
-import Particles from "react-tsparticles";
-import { loadFull } from "tsparticles";
-import { particleOptions } from "@/utils/particlesConfig";
 import AppContainer from "@/components/AppContainer/AppContainer";
 
 export default function Home() {
-  const [route, setRoute] = useState();
-  const options = particleOptions;
-
-  //Particles library configuration
-  const particlesInit = useCallback(async (main) => {
-    await loadFull(main);
-  });
-
   return (
     <main>
-      {/* <Particles
-        id="tsparticles"
-        options={options}
-        height="100vh"
-        width="100vw"
-        init={particlesInit}
-      /> */}
       <AppContainer />
     </main>
   );
--- src/app/signin/page.js
+++ src/app/signin/page.js
@@ -1,23 +1,11 @@
 "use client";
-//React
-import { useCallback, useState } from "react";
 
 //Libraries
 import "tachyons";
-import Particles from "react-tsparticles";
-import { loadFull } from "tsparticles";
-import { particleOptions } from "@/utils/particlesConfig";
 import SignIn from "@/components/Signin/SignIn";
 import { useRouter } from "next/navigation";
 
 export default function SignInPage() {
-  const [route, setRoute] = useState();
-  const options = particleOptions;
-
-  //Particles library configuration
-  const particlesInit = useCallback(async (main) => {
-    await loadFull(main);
-  });
   const router = useRouter();
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -25,17 +13,8 @@ export default function SignInPage() {
   };
 
   return (
-    <>
-      {/* <Particles
-        id="tsparticles"
-        options={options}
-        height="100vh"
-        width="100vw"
-        init={particlesInit}
-      /> */}
-      <div className="vh-100 flex items-center">
-        <SignIn handleSubmit={handleSubmit} />
-      </div>
-    </>
+    <div className="vh-100 flex items-center">
+      <SignIn handleSubmit={handleSubmit} />
+    </div>
   );
 }
