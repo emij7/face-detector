a74d2a2a47644b84fa5ea8a21b94e0aa796177e7
Emiliano Juarez <75501977+emij7@users.noreply.github.com>
2023-10-04 20:30:57 -0300

    removed next config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,1 @@
 /** @type {import('next').NextConfig} */
-
-module.exports = {
-  webpack5: true,
-  webpack: (config) => {
-    config.resolve.fallback = { fs: false };
-
-    return config;
-  },
-};
